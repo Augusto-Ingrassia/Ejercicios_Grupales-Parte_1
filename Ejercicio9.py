6b6a2588b9aba7e5e81f6b84de00bec66f22ad32
Lautaro Mejias <mejiaslauti@gmail.com>
2025-09-03 20:48:16 -0300

    Ejercicios del 8 al 20

--- Ejercicio9.py
+++
@@ -1,4 +0,0 @@
-#9- Recorre la cadena del ejercicio 6 y transforma cada carácter a su código ASCII.
-cadena = input("Ingresa una cadena: ")
-for caracter in cadena:
-    print(ord(caracter), end=" ")
